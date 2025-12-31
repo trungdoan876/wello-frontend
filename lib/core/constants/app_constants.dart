3d56cfc4a60a573b8f4438162b6ad8fa9e6930e0
trungdoan876 <trungdoan876@gmail.com>
2025-12-31 12:06:58 +0700

    update

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -2,5 +2,5 @@
 class AppConstants {
   // Cấu hình Google OAuth
   // Web Client ID từ cùng Google Cloud project với Android OAuth Client
-  static const String googleWebClientId = '708301638478-8omhqbj4og9r95tf9dg2r37b3bo05ign.apps.googleusercontent.com';
+  static const String googleWebClientId = '';
 }
